72d029c9e3536774268eebc6c6b6b65d83f91f33
cissoko97 <boriscissoko@gmail.com>
2019-11-29 12:00:48 +0100

    Add methods getTotalPrice on product models and uddate home view

--- models/Product.js
+++ models/Product.js
@@ -5,7 +5,13 @@ module.exports = (sequelize, DataTypes) => {
     price: DataTypes.INTEGER,
     issolid: DataTypes.BOOLEAN,
     quantity: DataTypes.INTEGER,
-  }, {});
+  }, {
+    getterMethods: {
+      getTotalPrice() {
+        return this.getDataValue('price') * this.getDataValue('quantity');
+      }
+    }
+  });
   Product.associate = function (models) {
     // associations can be defined here
     Product.belongsTo(models.User, {
